4cf81660360188a3bc9b900a72f34b0f3ed9f6ef
DERICK TEMFACK <81715817+tderick@users.noreply.github.com>
2022-10-26 07:49:11 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hr_add_company_to_attendence",
+    'name': "hr_add_company_to_attendence_without_compute",
 
     'summary': """Add the company address to every attendence records in a form of string""",
 
     'author': "DERICK TEMFACK",
-    'website': "https://github.com/tderick/hr_add_company_to_attendence",
+    'website': "https://github.com/tderick/hr_add_company_to_attendence_without_compute",
     'category': 'Uncategorized',
     'version': '0.1',
     # any module necessary for this one to work correctly
