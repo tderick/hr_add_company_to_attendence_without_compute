7265234a2e2c1ac3551fdcb594d17febc88c65bb
DERICK TEMFACK <81715817+tderick@users.noreply.github.com>
2022-10-26 07:48:10 +0100

    Update attendance.py

--- models/attendance.py
+++ models/attendance.py
@@ -4,8 +4,6 @@ from odoo import fields, models, api
 class InheritedAttendance(models.Model):
     _inherit = "hr.attendance"
 
-    agence = fields.Char(string="Agence", compute="_compute_agence")
+    agence = fields.Char(string="Agence")
 
-    def _compute_agence(self):
-        for attendance in self:
-            attendance.agence = self.env.user.company_id.street
+
